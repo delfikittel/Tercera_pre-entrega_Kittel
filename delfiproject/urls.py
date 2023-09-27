4db73b920e33836d164775f46d0d730083883923
delfkittel <delfikittel@gmail.com>
2023-09-26 23:01:09 -0300

    Version 2.1

--- delfiproject/urls.py
+++ delfiproject/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('Timeline.urls')),
+    path('Timeline/', include('Timeline.urls')),
 ]
